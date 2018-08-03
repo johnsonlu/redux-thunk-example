5c07adae7b0f6fce4d19cfaa9cee6f6287f6e24b
Johnson Lu <johnson.lu@ef.com>
2018-08-03 16:15:09 +0800

    Add reducers.

---
+++ src/reducers/items.js
@@ -0,0 +1,29 @@
+export function itemsHasErrored(state = false, action) {
+    switch (action.type) {
+        case 'ITEMS_HAS_ERRORED':
+            return action.itemsHasErrored;
+
+        default:
+            return state;
+    }
+}
+
+export function itemsIsLoading(state = false, action) {
+    switch (action.type) {
+        case 'ITEMS_IS_LOADING':
+            return action.itemsIsLoading;
+
+        default:
+            return state;
+    }
+}
+
+export function items(state = [], action) {
+    switch (action.type) {
+        case 'ITEMS_FETCH_DATA_SUCCESS':
+            return action.items;
+
+        default:
+            return state;
+    }
+}

35b8f6a95fcc2676e2dedb35b4d2395e50641268
Lu Jianxin <johnsonlu@outlook.com>
2018-08-06 20:24:57 +0800

    Add redux-logger for development env.

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -5,10 +5,19 @@ import {
 import thunk from "redux-thunk";
 import rootReducer from "../reducers";
 
+const middlewares = [];
+middlewares.push(thunk);
+
+if (process.env.NODE_ENV === `development`) {
+    const { logger } = require(`redux-logger`);
+
+    middlewares.push(logger);
+}
+
 export default function configureStore(initialState) {
     return createStore(
         rootReducer,
         initialState,
-        applyMiddleware(thunk)
+        applyMiddleware(...middlewares)
     );
 }
